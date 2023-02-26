bf556aea359693b9a0e2a7fa40df3c0791639e34
LuchoD99 <luciano.diazocampo@gmail.com>
2023-02-26 12:47:52 -0300

    partes de portfolio

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function navbar() {
+    return <div>navbar</div>;
+}
+//Debe tener Home, perfil(about), skills, proyectos, contactos
---
+++ src/components/Proyecto/proyectos.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function proyectos() {
+    return <div>proyectos</div>;
+}

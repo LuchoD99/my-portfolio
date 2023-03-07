89c696b492b01ff82e5f72ad9dec9b4eb1f5f1d3
LuchoD99 <luciano.diazocampo@gmail.com>
2023-03-07 14:31:10 -0300

    update navbar

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,6 +1,42 @@
 import React from 'react';
-
+import './navbar.css';
 export default function navbar() {
-    return <div>navbar</div>;
+    return (
+        <header>
+            <nav className="navbar container">
+                <a href="#" class="nav-link">
+                    LEDO
+                </a>
+                <ul className="nav-list">
+                    <li className="nav-item">
+                        <a href="#" className="nav-link">
+                            Home
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" className="nav-link">
+                            Perfil
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" className="nav-link">
+                            Tecnologia
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" className="nav-link">
+                            Proyectos
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" className="nav-link">
+                            Contactos
+                        </a>
+                    </li>
+                    <div className="active"></div>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 //Debe tener Home, perfil(about), skills, proyectos, contactos

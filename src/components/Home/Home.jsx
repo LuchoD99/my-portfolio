f52800ad836eec55188485716eea7a309d525573
LuchoD99 <luciano.diazocampo@gmail.com>
2023-03-22 14:22:52 -0300

    home cambios

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import './Home.css';
 export default function Home() {
     return (
-        <div className="home-center">
-            <h1>Developer FullStack</h1>
-            <h3>Luciano Ezequiel Diaz Ocampo</h3>
+        <div className="wrapper">
+            <div className="static-txt">Yo soy </div>
+            <ul className="dynamic-txts">
+                <li>
+                    <span>Luciano Ezequiel Diaz Ocampo</span>
+                </li>
+                <li>
+                    <span>un Developer FullStack</span>
+                </li>
+            </ul>
         </div>
     );
 }
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -10,7 +10,7 @@ export default function navbar() {
                 <ul className="nav-list">
                     <li className="nav-item">
                         <a href="#" className="nav-link">
-                            Home
+                            Inicio
                         </a>
                     </li>
                     <li className="nav-item">

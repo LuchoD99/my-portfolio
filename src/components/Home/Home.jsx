c569bb5f1d7a725af14c2ccc1e7f62ca39a17260
LuchoD99 <luciano.diazocampo@gmail.com>
2023-03-27 15:59:32 -0300

    home finish

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,15 +3,36 @@ import './Home.css';
 export default function Home() {
     return (
         <div className="wrapper">
-            <div className="static-txt">Yo soy </div>
-            <ul className="dynamic-txts">
-                <li>
-                    <span>Luciano Ezequiel Diaz Ocampo</span>
-                </li>
-                <li>
-                    <span>un Developer FullStack</span>
-                </li>
-            </ul>
+            <div className="fullstack">
+                <ul>
+                    <li className="rotate-scale-down">d</li>
+                    <li className="rotate-scale-down">e</li>
+                    <li className="rotate-scale-down">s</li>
+                    <li className="rotate-scale-down">a</li>
+                    <li className="rotate-scale-down">r</li>
+                    <li className="rotate-scale-down">r</li>
+                    <li className="rotate-scale-down">o</li>
+                    <li className="rotate-scale-down">l</li>
+                    <li className="rotate-scale-down">l</li>
+                    <li className="rotate-scale-down">a</li>
+                    <li className="rotate-scale-down">d</li>
+                    <li className="rotate-scale-down">o</li>
+                    <li className="rotate-scale-down">r</li>
+                    <li> </li>
+                    <li className="rotate-scale-down">f</li>
+                    <li className="rotate-scale-down">u</li>
+                    <li className="rotate-scale-down">l</li>
+                    <li className="rotate-scale-down">l</li>
+                    <li className="rotate-scale-down">s</li>
+                    <li className="rotate-scale-down">t</li>
+                    <li className="rotate-scale-down">a</li>
+                    <li className="rotate-scale-down">c</li>
+                    <li className="rotate-scale-down">k</li>
+                </ul>
+            </div>
+            <div className="nombre">
+                <h2>Luciano Ezequiel Diaz Ocampo</h2>
+            </div>
         </div>
     );
 }

b5fdb83ff5a0dcc265993db924b0b87aba00922c
LuchoD99 <luciano.diazocampo@gmail.com>
2023-03-27 19:15:28 -0300

    perfil update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar/navbar';
 import './App.css';
 import Home from './components/Home/Home';
+import Perfil from './components/Perfil/Perfil';
 function App() {
     return (
         <div className="App">
             <Navbar />
             <Home />
+            <Perfil />
         </div>
     );
 }
--- src/components/Perfil/perfil.jsx
+++ src/components/Perfil/perfil.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
-
-export default function perfil() {
-  return (
-    <div>perfil</div>
-  )
+import React from 'react';
+import image from '../image/face.jpg';
+import './perfil.css';
+import archivo from './archivo/CV-LUCIANO-DIAZ.pdf';
+export default function Perfil() {
+    return (
+        <div className="perfil">
+            <div className="text-info">
+                <h3 className="text">
+                    Soy un Full Stack Developer. Soy una persona trabajadora
+                    enfocado en el proyecto, soy joven y con mucha energía para
+                    aprender y mejorar durante el proyecto.
+                </h3>
+                <a className="descarga" href={archivo} download>
+                    CV
+                </a>
+            </div>
+            <div className="text-img">
+                <img className="imagenes" src={image} alt="img not found" />
+            </div>
+        </div>
+    );
 }
+//Sobre mi y foto y pdf de CV

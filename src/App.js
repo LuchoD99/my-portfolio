b431e230b0e74da8a51a1382a9b033bd3893402d
LuchoD99 <luciano.diazocampo@gmail.com>
2023-04-18 17:31:08 -0300

    cambios3.0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Navbar from './components/Navbar/navbar';
 import './App.css';
 import Home from './components/Home/Home';
-import Perfil from './components/Perfil/Perfil';
 import Skills from './components/Skill/Skills';
 import Proyectos from './components/Proyecto/proyectos';
 import Footer from './components/Footer/footer';
@@ -10,7 +9,6 @@ function App() {
         <div className="App">
             <Navbar />
             <Home />
-            <Perfil />
             <Skills />
             <Proyectos />
             <Footer />
